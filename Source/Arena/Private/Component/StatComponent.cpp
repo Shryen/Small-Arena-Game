19f2549ec351aee58bd3cc39375f89e58bddf4f7
Shryen <hbk9238@hotmail.com>
2026-01-01 23:10:49 +0100

    StatComponent with Health delegate

---
+++ Source/Arena/Private/Component/StatComponent.cpp
@@ -0,0 +1,23 @@
+﻿#include "Component/StatComponent.h"
+
+
+UStatComponent::UStatComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+
+void UStatComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	OnHealthChanged.Broadcast(Health, MaxHealth);
+}
+
+void UStatComponent::SetHealth(float NewHealth)
+{
+	Health = FMath::Clamp(NewHealth, 0.f, MaxHealth);
+    
+	OnHealthChanged.Broadcast(Health, MaxHealth);
+}
+
+
---
+++ Source/Arena/Public/Component/StatComponent.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "StatComponent.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnAttributeChangedSignature, float, CurrentValue, float, MaxValue);
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class ARENA_API UStatComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UStatComponent();
+
+	UPROPERTY(BlueprintAssignable, Category = "Events")
+	FOnAttributeChangedSignature OnHealthChanged;
+	
+	UFUNCTION(BlueprintCallable, Category = "Stats|Vital")
+	void SetHealth(float NewHealth);
+	
+protected:
+	virtual void BeginPlay() override;
+	
+	
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "Stats|Vital")
+	float Health = 0.f;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Stats|Vital")
+	float MaxHealth = 100.f;
+};
