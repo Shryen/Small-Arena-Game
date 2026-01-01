8ff1422493cbc4e2751236998736b2a9bdc51cb6
Shryen <hbk9238@hotmail.com>
2026-01-01 22:45:21 +0100

    PlayerController, GameMode, Inputs

---
+++ Source/Arena/Private/Controller/Player/ArenaPlayerController.cpp
@@ -0,0 +1,48 @@
+﻿#include "Controller/Player/ArenaPlayerController.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+
+void AArenaPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
+			Subsystem->AddMappingContext(InputMappingContext, 0);
+}
+
+void AArenaPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	if (UEnhancedInputComponent* EnhancedInput = Cast<UEnhancedInputComponent>(InputComponent))
+	{
+		EnhancedInput->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AArenaPlayerController::Move);
+		EnhancedInput->BindAction(LookAction, ETriggerEvent::Triggered, this, &AArenaPlayerController::Look);
+	}
+}
+
+void AArenaPlayerController::Move(const FInputActionValue& Value)
+{
+	const FVector2D Input = Value.Get<FVector2D>();
+	if (APawn* ControlledPawn = Cast<APawn>(GetPawn()))
+	{
+		// Get Rotation
+		const FRotator Rotation = GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		// Get directions by rotation
+		const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+		// Apply Movement
+		ControlledPawn->AddMovementInput(ForwardDirection, Input.Y); // Back and forth
+		ControlledPawn->AddMovementInput(RightDirection, Input.X);	 // Right and Left
+	}
+}
+
+void AArenaPlayerController::Look(const FInputActionValue& Value)
+{
+	const FVector2D Input = Value.Get<FVector2D>();
+	AddPitchInput(-Input.Y);
+	AddYawInput(Input.X);
+}
+
---
+++ Source/Arena/Public/Controller/Player/ArenaPlayerController.h
@@ -0,0 +1,33 @@
+﻿
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "ArenaPlayerController.generated.h"
+
+struct FInputActionValue;
+/**
+ * 
+ */
+UCLASS()
+class ARENA_API AArenaPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+
+	
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "Arena|Input")
+	TObjectPtr<class UInputMappingContext> InputMappingContext;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Arena|Input|Actions")
+	TObjectPtr<class UInputAction> MoveAction;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Arena|Input|Actions")
+	TObjectPtr<class UInputAction> LookAction;
+	
+	void Move(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
+};
