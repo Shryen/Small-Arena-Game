91939b7048168227d5ae4f39e53e8049e49cc3e3
Shryen <hbk9238@hotmail.com>
2026-01-01 22:18:44 +0100

    GameMode, StartLevel, Project Settings

---
+++ Source/Arena/Public/GameMode/ArenaGameMode.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ArenaGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARENA_API AArenaGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
